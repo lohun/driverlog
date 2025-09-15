17c0597582e677914354d8962f26c9e973a9f452
Nwankpele Damilola <nwankpele@gmail.com>
2025-09-15 13:53:10 +0100

    cors

--- driverlog/settings.py
+++ driverlog/settings.py
@@ -99,7 +99,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Vite default port
     "http://127.0.0.1:5173",
     "http://localhost:5174",
-    "http://127.0.0.1:5174"
+    "http://127.0.0.1:5174",
+    "https://eldlogs-sooty.vercel.app"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
